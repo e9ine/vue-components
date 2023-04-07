1833a0e1cec9dd3f0e372eba622aa84d9749aca9
e9ine <e9ine@e9ines-MacBook-Pro.local>
2023-04-07 09:55:56 +0530

    chore: remove vue2-google-maps package

--- docs/.vuepress/components/GoogleAddressFinderExample.vue
+++ docs/.vuepress/components/GoogleAddressFinderExample.vue
@@ -7,16 +7,7 @@
 
 <script>
 import Vue from 'vue';
-import * as VueGoogleMaps from 'vue2-google-maps';
 import AddressFinder from '@/components/address-finder/Component.vue';
-
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: import.meta.env.VUE_APP_GOOGLE_KEY,
-        libraries: 'places'
-    },
-    installComponents: false
-});
 export default {
     name: 'GoogleAddressFinderExample',
     components: {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from '@/app/router';
-import * as VueGoogleMaps from 'vue2-google-maps';
 import jQuery from 'jquery';
 
 import {store} from '@/app/store';
@@ -10,13 +9,6 @@ import './filters';
 Vue.config.productionTip = false;
 
 window.$ = window.jQuery = jQuery;
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: import.meta.env.VUE_APP_GOOGLE_KEY,
-        libraries: 'places'
-    },
-    installComponents: false
-});
 new Vue({
     render: (h) => h(App),
     router,
